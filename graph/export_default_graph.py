47ff5bb21b66c54745971156b92d7dbf29277049
Jacques7103 <ferdinand.jacques@binus.ac.id>
2023-06-06 20:54:23 +0700

    drafted export and custom

---
+++ graph/export_default_graph.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+height = 10
+g = 9.8
+mass = 1.0
+
+def export_default_graph(mass_a,mass_b,mass_c,mass_d,g,height,filename):
+    def linear_curve(t):
+        return height - (mass_a * g) * t
+
+    def parabolic_curve(t):
+        end_point_x = np.sqrt(2 * height / (mass_b * g))
+        slope = -height / end_point_x
+        a = -height / (end_point_x**2)
+        return 2 * (slope * t + height) - (a * t**2 + height)
+
+    def circular_curve(t):
+        end_point_x = 2 * np.sqrt(height / (mass_c * g))
+        slope = -height / end_point_x
+        a = -height / (end_point_x**2)
+        return 2 * (slope * t + height) - (a * t**2 + height)
+
+    def cycloid_curve(t):
+        end_point_x = (2 * height / np.pi) * np.sqrt(np.pi / (2 * (mass_d * g) * height))
+        slope = -height / end_point_x
+        a = -height / (end_point_x**2)
+        return 2 * (slope * t + height) - (a * t**2 + height)
+    
+    linear_time_to_reach_zero = height / (mass_a * g)
+    parabolic_time_to_reach_zero = np.sqrt(2 * height / (mass_b * g))
+    circular_time_to_reach_zero = 2 * np.sqrt(height / (mass_c * g))
+    cycloid_time_to_reach_zero = (2 * height / np.pi) * np.sqrt(np.pi / (2 * (mass_d * g) * height))
+
+    t = np.linspace(0, max(linear_time_to_reach_zero, parabolic_time_to_reach_zero, circular_time_to_reach_zero, cycloid_time_to_reach_zero), 100)
+
+    fig, ax = plt.subplots()
+
+    ax.plot(t, np.maximum(linear_curve(t), 0), 'b-', label='Linear')
+    ax.plot(t, np.maximum(parabolic_curve(t), 0), 'g-', label='Parabolic')
+    ax.plot(t, np.maximum(circular_curve(t), 0), 'r-', label='Circular')
+    ax.plot(t, np.maximum(cycloid_curve(t), 0), 'm-', label='Cycloidal')
+
+    ax.plot(linear_time_to_reach_zero, 0, 'bo', label=f'Linear: {linear_time_to_reach_zero:.2f}s')
+    ax.plot(parabolic_time_to_reach_zero, 0, 'go', label=f'Parabolic: {parabolic_time_to_reach_zero:.2f}s')
+    ax.plot(circular_time_to_reach_zero, 0, 'ro', label=f'Circular: {circular_time_to_reach_zero:.2f}s')
+    ax.plot(cycloid_time_to_reach_zero, 0, 'mo', label=f'Cycloidal: {cycloid_time_to_reach_zero:.2f}s')
+
+    ax.set_ylim(0, height)
+    ax.set_xlim(0, max(linear_time_to_reach_zero, parabolic_time_to_reach_zero, circular_time_to_reach_zero, cycloid_time_to_reach_zero))
+
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Height')
+    ax.legend()
+
+    plt.savefig(f'result/{filename}')
+
+    return True
---
+++ page/custom_graph.py
@@ -0,0 +1,62 @@
+import customtkinter
+from tkinter import messagebox
+import graph.visualize_custom_graph as vc
+
+def custom_graph(app):
+
+    custom_graph_frame = customtkinter.CTkFrame(master=app, width=300, height=300,corner_radius=0)
+    
+    label = customtkinter.CTkLabel(custom_graph_frame, text="Custom Graphs")
+    label.pack(pady=(10,0))
+    
+    mass_a_input = customtkinter.CTkTextbox(custom_graph_frame, height=10)
+    mass_a_input.insert("0.0", "Mass")
+    mass_a_input.pack(pady=(10,0), padx=(0,10))
+    
+    gravity_input = customtkinter.CTkTextbox(custom_graph_frame, height=10)
+    gravity_input.insert("0.0", "Gravity")
+    gravity_input.pack(pady=(10,0), padx=(0,10))
+    
+    height_input = customtkinter.CTkTextbox(custom_graph_frame, height=10)
+    height_input.insert("0.0", "Height")
+    height_input.pack(pady=(10,0), padx=(0,10))
+    
+    background_color_option = customtkinter.CTkOptionMenu(custom_graph_frame, values=["red", "green",'blue'])
+    background_color_option.pack(pady=(10,0), padx=10)
+    
+    curve_option = customtkinter.CTkOptionMenu(custom_graph_frame, values=["linear curve", "parabolic curve",'circular curve','cycloid curve'])
+    curve_option.pack(pady=(10,0), padx=10)
+    
+    def validate_input(input_value, message):
+        try:
+            int_value = int(input_value)
+            return int_value
+        except ValueError:
+            try:
+                float_value = float(input_value)
+                return float_value
+            except ValueError:
+                messagebox.showerror("Error", message)
+                return None
+    
+    def handle_load_graph():
+        if validate_input(mass_a_input.get('1.0', 'end-1c'), 'mass must be a valid number!') is None:
+            return
+        if validate_input(gravity_input.get('1.0', 'end-1c'), 'gravity must be a valid number!') is None:
+            return
+        if validate_input(height_input.get('1.0', 'end-1c'), 'height must be a valid number!') is None:
+            return
+        
+        mass = int(float(mass_a_input.get('1.0', 'end-1c')))
+        g = float(gravity_input.get('1.0', 'end-1c'))
+        height = float(height_input.get('1.0', 'end-1c'))
+        color = background_color_option.get()
+        curve = curve_option.get()
+        
+        vc.visualize_custom_graph(mass, g, height, color, curve)
+
+        
+    save_and_load_graph_button = customtkinter.CTkButton(custom_graph_frame, text="Load Graph", command=handle_load_graph)
+    save_and_load_graph_button.pack(pady=(10,10), padx=10)
+    
+    return custom_graph_frame
